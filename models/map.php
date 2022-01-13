55ba468afb3e511e1c54fbca0d7b54eb2aa65609
TaigaYamada-cell <daikonsukitarou@gmail.com>
2022-01-13 15:01:06 +0900

    空欄チェック機能を実装

--- models/map.php
+++ models/map.php
@@ -38,16 +38,22 @@ function search($data){
 }
 
 function registerComment($comment){
-    $pdo = connect();
-    try{
-        $statement = $pdo->prepare("INSERT INTO comment(text, user_id, post_id, created_at) VALUES (:text, :user_id, :post_id, CURRENT_TIMESTAMP)");
-        $statement->bindValue(":text", $comment["text"], PDO::PARAM_STR);
-        $statement->bindValue(":user_id", $comment["user_id"], PDO::PARAM_STR);
-        $statement->bindValue(":post_id", $comment["post_id"], PDO::PARAM_INT);
-        $statement->execute();
-        return "コメント登録に成功しました";
-    } catch(PDOExcepion $e){
-        $e->getMessage();
+        if(!empty($comment["text"])){
+        $pdo = connect();
+        try{
+            $statement = $pdo->prepare("INSERT INTO comment(text, user_id, post_id, created_at) VALUES (:text, :user_id, :post_id, CURRENT_TIMESTAMP)");
+            $statement->bindValue(":text", $comment["text"], PDO::PARAM_STR);
+            $statement->bindValue(":user_id", $comment["user_id"], PDO::PARAM_STR);
+            $statement->bindValue(":post_id", $comment["post_id"], PDO::PARAM_INT);
+            $statement->execute();
+            echo "コメント登録に成功しました。<br>前の画面に戻ってください。";
+        } catch(PDOExcepion $e){
+            $e->getMessage();
+        }
+    }elseif(empty($comment["user_id"])){
+        echo "コメントにログインが必要です。<br>前の画面に戻ってください。";
+    }else{
+        echo "空欄はコメント登録できません。<br>前の画面に戻ってください。";
     }
 }
 
